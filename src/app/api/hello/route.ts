7b76eceebe13246a4f99cf3b7ab3938a4bc2b2cf
Matthew Tao <matthew.yingtao@gmail.com>
2023-03-11 10:38:49 +1300

    iasd

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,20 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+	const textCompletion = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: "Greet me in french",
+		temperature: 0.6,
+	});
+
+  const text = completion.data.choices[0].text;
+
+  const imageGen = await openai.create({
+
+	return new Response(text);
 }
