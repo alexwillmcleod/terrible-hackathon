479bb227836a68a8b1edb86767861ba4d96e4331
Alexander McLeod <alexwillmcleod@gmail.com>
2023-03-11 18:09:25 +1300

    Changed fetch request address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 
 async function getData() {
-  const res = await fetch('http://localhost:3000/api/rss');
+  const res = await fetch('/api/rss');
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data');
