de4477a75a402618598f8228bde5a9e875fc31c2
Alexander McLeod <alexwillmcleod@gmail.com>
2023-03-11 21:58:47 +1300

    Updated README

--- src/app/api/rss/route.ts
+++ src/app/api/rss/route.ts
@@ -5,7 +5,6 @@ import { Configuration, OpenAIApi } from 'openai';
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
 
 const prisma = new PrismaClient();
 
